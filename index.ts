7d3d433009f811796d40c7bbccfdad3cccc44c69
wojoud2007 <116985103+wojoud2007@users.noreply.github.com>
2022-10-30 01:35:32 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -1,5 +1,1 @@
-//######################################################################
-//###########################           SSTech       ###################
-//###########################   Programming Courses  ###################
-
-console.log("salam ana wojoud sakn fi azemour ka9ea fi molay box3ib ou tahiya l ga3 li ghadi nwrihoum had project dyali ou an afrhan hit hada ghadi ikoun awal projectdyali ou ghadi nsr3o inxa2lah ou nwli xi hacker akhla9i nkhdm m3a xi xarikat inxa2lah ou kanbghikoum kamlin ");
+console.log('salam lbs 3likoum');
